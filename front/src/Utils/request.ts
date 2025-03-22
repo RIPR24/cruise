cea8da632813924cc6383a13c4a3141203012f0a
Bapiya <bapiya1@gmail.com>
2025-03-22 21:18:38 +0530

    api

--- front/src/Utils/request.ts
+++ front/src/Utils/request.ts
@@ -1,4 +1,4 @@
-const site = "http://localhost:8080/";
+const site = "https://cruise-api-ftfl.onrender.com/";
 const postReq = async (surl: string, data: {}) => {
   const res = await fetch(site + surl, {
     method: "POST",
